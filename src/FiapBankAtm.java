35d0f48154897234248e955df5fc2e1c6e5bf904
Arthur <a.arthuraraujo1711@gmail.com>
2026-04-02 18:51:41 -0300

    feat: adiciona cadastro de senha

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
 public class FiapBankAtm {
-
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+        String senhaCadastrada;
+
         System.out.println("=====================");
         System.out.println("==== Bem-Vindo! =====");
         System.out.println("=== FIAP BANK ATM ===");
         System.out.println("=====================");
+
+        System.out.print("Cadastre uma senha: ");
+        senhaCadastrada = scanner.nextLine();
     }
 }
