42f623f346985b36af0799cd069109c61afd9c50
Arthur <a.arthuraraujo1711@gmail.com>
2026-04-02 18:33:20 -0300

    feat: adiciona tela de boas-vindas do FIAP Bank ATM

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -4,5 +4,9 @@ public class FiapBankAtm {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("=====================");
+        System.out.println("==== Bem-Vindo! =====");
+        System.out.println("=== FIAP BANK ATM ===");
+        System.out.println("=====================");
     }
 }
