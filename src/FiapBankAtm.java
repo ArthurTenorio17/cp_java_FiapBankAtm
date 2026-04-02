03cbfce45fc8ae49f7712fd995c5a82dc8f3aa05
Arthur <a.arthuraraujo1711@gmail.com>
2026-04-02 18:57:23 -0300

    feat: valida senha com 6 dígitos numéricos

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -6,13 +6,23 @@ public class FiapBankAtm {
         Scanner scanner = new Scanner(System.in);
 
         String senhaCadastrada;
+        boolean senhaValida;
 
         System.out.println("=====================");
         System.out.println("==== Bem-Vindo! =====");
         System.out.println("=== FIAP BANK ATM ===");
         System.out.println("=====================");
 
-        System.out.print("Cadastre uma senha: ");
-        senhaCadastrada = scanner.nextLine();
+        do {
+            System.out.print("Cadastre uma senha (6 dígitos): ");
+            senhaCadastrada = scanner.nextLine();
+
+            senhaValida = senhaCadastrada.matches("\\d{6}");
+
+            if (!senhaValida) {
+                System.out.println("Senha inválida!");
+            }
+
+        } while (!senhaValida);
     }
 }
