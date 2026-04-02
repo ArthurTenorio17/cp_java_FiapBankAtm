9556bf4adbfc1e7de094fe560fd063687b1bb085
Arthur <a.arthuraraujo1711@gmail.com>
2026-04-02 19:04:52 -0300

    feat: impede repetição de dígitos na senha

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class FiapBankAtm {
+
+    public static boolean temRepetido(String senha) {
+        for (int i = 0; i < senha.length(); i++) {
+            for (int j = i + 1; j < senha.length(); j++) {
+                if (senha.charAt(i) == senha.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -14,10 +26,10 @@ public class FiapBankAtm {
         System.out.println("=====================");
 
         do {
-            System.out.print("Cadastre uma senha (6 dígitos): ");
+            System.out.print("Cadastre uma senha (6 dígitos, sem repetição): ");
             senhaCadastrada = scanner.nextLine();
 
-            senhaValida = senhaCadastrada.matches("\\d{6}");
+            senhaValida = senhaCadastrada.matches("\\d{6}") && !temRepetido(senhaCadastrada);
 
             if (!senhaValida) {
                 System.out.println("Senha inválida!");
